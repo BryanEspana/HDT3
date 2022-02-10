8226d54519564d6080685d5a8be6af6948274c74
Bryan España <esp21550@uvg.edu.gt>
2022-02-10 17:07:19 -0600

    Agregamos 4 clases y arrays

--- src/LectorDatos.java
+++ src/LectorDatos.java
@@ -1,10 +1,13 @@
 import java.io.*;
 import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
 public class LectorDatos {
     public void Escribir(String nombre){
         File f;
         FileWriter w;
         BufferedWriter bw;
+        ArrayList<String> datos= new ArrayList();
         PrintWriter wr;
 
         try{
@@ -13,16 +16,18 @@ public class LectorDatos {
             bw = new BufferedWriter(w);
             wr = new PrintWriter(bw);
             int min = 1;
-		    int max = 3000;
+		    int max = 10;
             int i;
-            String texto = "";
+            String texto = " ";
 		    for(i = min; i <=max; i++) {
 			    int getRandomValue = (int) (Math.random()*(max-min)) + min;
 			    texto += String.valueOf(getRandomValue+" ");
             }
             wr.write(texto);
+            datos.add(texto);
             wr.close();
             bw.close();
+            System.out.println(datos);
         }catch(Exception e){
             JOptionPane.showMessageDialog(null,"Ha ocurrido un error "+ e);
         }
---
+++ src/mergeSort.java
@@ -0,0 +1,10 @@
+/*
+* Universidad del Valle de Guatemala
+* @author Bryan Espana 21550 | Javier Prado - 21486 | Angel Perez - 21298
+* Algoritmos y estructura de datos
+* Seccion 10 
+*/
+
+public class mergeSort{
+
+}

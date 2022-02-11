fdd96547c437d5b7a24412a3504bcf25daa45fb4
Bryan España <esp21550@uvg.edu.gt>
2022-02-10 20:56:58 -0600

    Implementación de Merge & Gnome

--- src/mergeSort.java
+++ src/mergeSort.java
@@ -6,5 +6,23 @@
 */
 
 public class mergeSort{
-
+    public static void mrgSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+    
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mrgSort(l, mid);
+        mrgSort(r, n - mid);
+    
+        merge(a, l, r, mid, n - mid);
+    }
 }
